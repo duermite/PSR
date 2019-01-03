7e1e3fd3aa466812254500b6a7dd6e3fec35b46f
Liz Duermit <duermite@gmail.com>
2019-01-03 15:37:14 -0500

    Added script that includes ideas only, for how to deal will messy refs doc and lookup refs

---
+++ scripts/refs_lookup.R
@@ -0,0 +1,19 @@
+#Refs
+
+#Two ways to append full citation information to other tables
+
+#1
+#Loop lookup on each row
+#this is probably more practical
+#pull out path_abbr and ref
+#for each row one at a time
+#split refs on ","
+#lookup in refs table
+#put them back together
+#put back into table
+
+
+
+
+#2
+#Make new table with rows for each path_abbr and ref
