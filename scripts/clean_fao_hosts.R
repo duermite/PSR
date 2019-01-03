ceff50d79e3cab518d91cb014b17759e6d08dfc9
Liz Duermit <duermite@gmail.com>
2019-01-03 15:55:57 -0500

    Planned tasks in script

--- scripts/clean_fao_hosts.R
+++ scripts/clean_fao_hosts.R
@@ -1,9 +1,25 @@
 ##ALL CLEANING FOR FAO_HOSTS FILE
 library(dplyr)
+hosts <- read.csv("raw_data/fao_hosts.csv")
+db_search <- read.csv("raw_data/db_search.csv")
+host_depth <- read.csv("raw_data/host_depth.csv")
+host_extent <- read.csv("raw_data/host_extent.csv")
+host_fishery <- read.csv("raw_data/host_fishery.csv")
+host_size <- read.csv("raw_data/host_size.csv")
+invasives <- read.csv("raw_data/invasives.csv")
+sociality <- read.csv("raw_data/sociality.csv")
 
 #Tasks
 ##1-Change host type to something recognizeable
-##2-
+##2-Add data
+###db_search
+###host_depth
+###host_extent
+###host_fishery
+###host_size
+###invasives
+###sociality
+##3-save to clean data folder
 
 
 
@@ -27,6 +43,10 @@ calling_host_type <- function(order){
   return(host_type)
 }
 #adjusting family and host type
-decapod_hosts_clean <- decapod_hosts_clean %>% 
+hosts <- hosts %>% 
   mutate(family=tolower(Family_Group)) %>% 
   mutate(host_type=unlist(lapply(Order_Group,calling_host_type))) #need lapply so work for each row separately
+#haven't deleted original two columns yet
+
+write.csv(hosts,"clean_data/hosts_clean.csv",
+          row.names=FALSE)
