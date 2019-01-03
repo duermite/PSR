6349b3bda351c0ec37262e6918b289ed39cbb19d
Liz Duermit <duermite@gmail.com>
2019-01-03 15:47:11 -0500

    Added in other tasks to be completed

--- scripts/clean_pathogen_hosts.R
+++ scripts/clean_pathogen_hosts.R
@@ -1,6 +1,14 @@
-###Checking for duplicates##
+###ALL CLEANING TASKS FOR PATHOGEN_HOSTS FILE###
 library(dplyr)
 
+#Tasks
+##1-Dup check
+##2-Make sure all path_abbr are in pathogen
+##3-Verify all host_genus_species in fao_hosts, remove if not
+##4-complete in_wild to best of ability??
+
+#1
+###Checking for duplicates##
 #import and check for duplicates in pathogen_hosts file
 
 pathogen_hosts <- read.csv("raw_data/pathogen_hosts.csv")
