954d4f95cd961991cfac57d180aecf3d3c921eed
Liz Duermit <duermite@gmail.com>
2019-01-08 17:12:21 -0500

    Wrote out tasks to complete

--- scripts/clean_invasives.R
+++ scripts/clean_invasives.R
@@ -7,3 +7,12 @@ invasives <- read.csv("raw_data/invasives_raw.csv")
 fao_hosts <- read.csv("raw_data/fao_hosts.csv") #is this needed?
 
 #Tasks
+#1-Find duplicates and combine info
+#2-Create new status/impact to denote only high impact spp?
+#3-Fix sources and add to reference list
+#4-Check how many hosts are listed and ensure list is complete
+#5-Use Environment data to check fao_hosts?
+#final-Save to clean data folder
+
+#1-
+#Find duplicates and combine info
