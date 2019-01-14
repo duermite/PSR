8778e29e6c0bff5c24810a550e995e16146ba7c4
Liz Duermit <duermite@gmail.com>
2019-01-14 12:36:22 -0500

    Found dups, combined info, script to remove semicolons

--- scripts/clean_invasives.R
+++ scripts/clean_invasives.R
@@ -36,17 +36,44 @@ invasives <- invasives %>%
   separate(Taxon, c("genus","species"),sep=" ") %>% 
   unite("host_genus_species",c("genus","species"),sep="_")
 #join the two tables
-invasives_clean <- right_join(invasives,fao_hosts, by="host_genus_species") %>% 
+invasives2 <- right_join(invasives,fao_hosts, by="host_genus_species") %>% 
   filter(Source!="NA") %>% 
   select(1:8) 
 #test that extras are really gone
-hosts_in_clean_invasives <- invasives_clean %>% 
+hosts_in_clean_invasives <- invasives2 %>% 
   arrange(host_genus_species) %>% 
   distinct(host_genus_species)
 setdiff(hosts_in_clean_invasives,hosts_in_fao_hosts) #empty
 
 #1-
 #Find duplicates and combine info
-dup_search <- data.frame(table(invasives$Taxon))
+dup_search <- data.frame(table(invasives2$host_genus_species))
 dup_search <- dup_search[dup_search$Freq>1,]
 dup_search
+#remove NAs
+invasives2[is.na(invasives2)] <- ""
+invasives3 <- invasives2 %>% 
+  group_by(host_genus_species) %>% 
+  summarize(inv_in=paste(Invasive.In,collapse="; "),
+            impact=paste(Status.Impact,collapse="; "),
+            initial_dist=paste(Initial.Distribution,collapse="; "),
+            latest_dist=paste(Latest.update.distribution,collapse="; "),
+            source=paste(Source,collapse="; "),
+            env=paste(Environment,collapse="; ")) 
+#remove ; where not needed
+rem_semicolon <- function(x){
+  substr(x, 1 + (1 * as.numeric(substr(x,1,1)==';')), 
+         nchar(x) - (1 * as.numeric(substr(x, nchar(x), 
+                                           nchar(x)) == ';')))
+}
+test <- c("words; words", ";", ";words","words;")
+rem_semicolon(test) #it works! 
+#apply this function throughout the dataframe...
+invasives4 <- invasives3 %>% 
+  rowwise() %>% 
+  mutate(inv_in=rem_semicolon(inv_in),
+         impact=rem_semicolon(impact),
+         initial_dist=rem_semicolon(initial_dist),
+         latest_dist=rem_semicolon(latest_dist),
+         env=rem_semicolon(env))
+#nearly there, a few random ; but they will have to stay for now
