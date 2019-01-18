4ca9131cecd01ab426a31e560daba1d77aa752c2
Liz Duermit <duermite@gmail.com>
2019-01-18 13:07:53 -0500

    function to fill in sociality with family score if no species score

--- scripts/clean_sociality.R
+++ scripts/clean_sociality.R
@@ -20,5 +20,21 @@ social_aqua <- read.csv("raw_data/social_aquanis.csv")
 #4: completely (or mostly) gregarious
 ##doing this in excel, with version control in Git
 
+##2-
+#Fill in species from fam, indicate which are interpolated somehow
+interpolate_social <- function(social_score,fam_score){
+  if(social_score>0){
+    fam_soc_score <- social_score
+  } else{
+    fam_soc_score <- fam_score
+  }
+  return(fam_soc_score)
+}
+
+
+
+
+
+
 write.csv(sociality,"clean_data/sociality_clean.csv",
           row.names=FALSE)
