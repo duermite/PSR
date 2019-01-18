c0a69fd44e5ccff0118e8e37ce695993c014e14d
Liz Duermit <duermite@gmail.com>
2019-01-18 12:24:39 -0500

    Describe sociality score in script and make judgement, adding scores to excel sheet

--- scripts/clean_sociality.R
+++ scripts/clean_sociality.R
@@ -1,19 +1,24 @@
 ##ALL CLEANING TASKS FOR SOCIALITY HERE##
 
 library(dplyr)
+library(tidyr)
 sociality_sp <- read.csv("raw_data/sociality_sp.csv")
 sociality_fam <- read.csv("raw_data/sociality_fam.csv")
+social_aqua <- read.csv("raw_data/social_aquanis.csv")
 
 
 #TASKS
 ##1-Create a scale for sociality and define
 ##2-Fill in species from fam, indicate which are interpolated somehow
 
-
-
-
-
-
+##1-
+#Create a scale for sociality and define, 0-4
+#0: unknown
+#1: completely solitary, can include dense pops that are aggressive
+#2: reproductive aggregations
+#3: ontonetic agg., some life stage is gregarious
+#4: completely (or mostly) gregarious
+##doing this in excel, with version control in Git
 
 write.csv(sociality,"clean_data/sociality_clean.csv",
           row.names=FALSE)
