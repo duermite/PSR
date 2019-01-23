4d0afa0ea784d93a32d661535b83640e368133cb
Liz Duermit <duermite@gmail.com>
2019-01-23 13:40:27 -0500

    Notes and some completion of virus_type

--- scripts/clean_pathogen.R
+++ scripts/clean_pathogen.R
@@ -21,3 +21,20 @@ path_transmission <- read.csv("raw_data/path_transmission.csv")
 pathogen2 <- left_join(pathogen,path_transmission,by="pathogen_type") %>% 
   unite(ref,c("ref.x","ref.y"),sep=", ") %>% 
   select(1:6,13,7:10)
+
+#3-
+#check for path_abbr dups
+dup_search <- data.frame(table(pathogen2$path_abbr))
+dup_search <- dup_search[dup_search$Freq>1,]
+dup_search
+#no duplicates!!
+
+#4-
+#complete virus_type and QAQC
+#completing this in the excel spreadsheet
+#did this to a minor extent
+#note: virus_type is sometimes ORder and sometimes Family
+
+
+
+
