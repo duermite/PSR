637f3fadf2d5cd1e7dac79d0c83662a5af034b73
Liz Duermit <duermite@gmail.com>
2019-01-25 18:29:24 -0500

    Created new script for glm

---
+++ scripts/analyze_glm.R
@@ -0,0 +1,7 @@
+#GLM Analysis of dataset
+
+decapods <- read.csv("analyze_data/hosts_clean_pathcounts.csv")
+path_hosts <- read.csv("clean_data/path_hosts_clean.csv")
+pathogens <- read.csv("clean_data/pathogen_clean.csv")
+
+#Tasks
