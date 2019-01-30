ac524357bb1aafe9bb548405d8e04de8e211804e
Liz Duermit <duermite@gmail.com>
2019-01-30 11:16:24 -0500

    Changed social score to useful names

--- scripts/clean_sociality.R
+++ scripts/clean_sociality.R
@@ -10,6 +10,8 @@ social_aqua <- read.csv("raw_data/social_aquanis.csv")
 #TASKS
 ##1-Create a scale for sociality and define
 ##2-Fill in species from fam, indicate which are interpolated somehow
+##3-change numbers to something meaningful
+#final-save to clean data folder
 
 ##1-
 #Create a scale for sociality and define, 0-4
@@ -19,6 +21,8 @@ social_aqua <- read.csv("raw_data/social_aquanis.csv")
 #3: ontonetic agg., some life stage is gregarious
 #4: completely (or mostly) gregarious
 ##doing this in excel, with version control in Git
+
+
 
 ##2-
 #Fill in species from fam, indicate which are interpolated somehow
@@ -41,8 +45,29 @@ sociality_sp2 <- left_join(sociality_sp,sociality_fam,by="Family_Group") %>%
          notes=notes.x,
          ref=ref.x)
 
+##3-change numbers to something meaningful
+social_naming <- function(social_score){
+  if(social_score==1){
+    social_name <- "solitary"
+  } else if(social_score==2){
+    social_name <- "repro_aggregate"
+  } else if(social_score==3){
+    social_name <- "ontogenetic_gregarious"
+  } else if(social_score==4){
+    social_name <- "gregarious"
+  } else{
+    social_name <- "unknown"
+  }
+  return(social_name)
+}
+#test function
+social_naming(0)
 
+sociality_sp3 <- sociality_sp2 %>% 
+  rowwise() %>% 
+  mutate(social_score=social_naming(social_score)) %>% 
+  mutate(fam_soc_score=social_naming(fam_soc_score))
 
-
-write.csv(sociality_sp2,"clean_data/sociality_clean.csv",
+#final-save to clean data folder
+write.csv(sociality_sp3,"clean_data/sociality_clean.csv",
           row.names=FALSE)
