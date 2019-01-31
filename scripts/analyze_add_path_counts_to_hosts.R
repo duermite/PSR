e8f88df9fba46b43b9b34cf2119879f78e1b1d0e
Liz Duermit <duermite@gmail.com>
2019-01-31 16:50:39 -0500

    Added relevant 0s to datasets in scripts and saved anew

--- scripts/analyze_add_path_counts_to_hosts.R
+++ scripts/analyze_add_path_counts_to_hosts.R
@@ -37,7 +37,8 @@ colnames(num_rhiz) <- c("host_genus_species","num_rhiz")
 
 #count number of bacteria per host
 num_bacteria <- data.frame(table(
-  path_type_join_hosts$host_genus_species[path_type_join_hosts$pathogen_type=="bacteria_extra"|path_type_join_hosts$pathogen_type=="bacteria_intra"]))
+  path_type_join_hosts$host_genus_species[path_type_join_hosts$pathogen_type=="bacteria_extra"|
+                                            path_type_join_hosts$pathogen_type=="bacteria_intra"]))
 colnames(num_bacteria) <- c("host_genus_species","num_bacteria")
 
 #count number of intracellular bacteria per host
@@ -45,10 +46,40 @@ num_bacteria_intra <- data.frame(table(
   path_type_join_hosts$host_genus_species[path_type_join_hosts$pathogen_type=="bacteria_intra"]))
 colnames(num_bacteria_intra) <- c("host_genus_species","num_bacteria_intra")
 
+#count number of extracellular bacteria per host
+num_bacteria_extra <- data.frame(table(
+  path_type_join_hosts$host_genus_species[path_type_join_hosts$pathogen_type=="bacteria_extra"]))
+colnames(num_bacteria_extra) <- c("host_genus_species","num_bacteria_extra")
+
 #count number of microsporidians per host
 num_microsp <- data.frame(table(
   path_type_join_hosts$host_genus_species[path_type_join_hosts$pathogen_type=="microsporidian"]))
 colnames(num_microsp) <- c("host_genus_species","num_microsp")
+
+#count apicomplexans
+num_api <- data.frame(table(
+  path_type_join_hosts$host_genus_species[path_type_join_hosts$pathogen_type=="apicomplexan"]))
+colnames(num_api) <- c("host_genus_species","num_api")
+
+#count cestodes
+num_cestode <- data.frame(table(
+  path_type_join_hosts$host_genus_species[path_type_join_hosts$pathogen_type=="cestode"]))
+colnames(num_cestode) <- c("host_genus_species","num_cestode")
+
+#count fungi
+num_fungi <- data.frame(table(
+  path_type_join_hosts$host_genus_species[path_type_join_hosts$pathogen_type=="fungi"]))
+colnames(num_fungi) <- c("host_genus_species","num_fungi")
+
+#count nematodes
+num_nematode <- data.frame(table(
+  path_type_join_hosts$host_genus_species[path_type_join_hosts$pathogen_type=="nematode"]))
+colnames(num_nematode) <- c("host_genus_species","num_nematode")
+
+#count trematodes
+num_trematode <- data.frame(table(
+  path_type_join_hosts$host_genus_species[path_type_join_hosts$pathogen_type=="trematode"]))
+colnames(num_trematode) <- c("host_genus_species","num_trematode")
 
 #count by path transmission type
 num_direct <- data.frame(table(
@@ -92,7 +123,13 @@ decapod_hosts2 <- left_join(decapod_hosts,num_pathogen,by="host_genus_species") 
   left_join(num_obligate,by="host_genus_species") %>% 
   left_join(num_opportunist,by="host_genus_species") %>% 
   left_join(num_macro,by="host_genus_species") %>% 
-  left_join(num_micro,by="host_genus_species") 
+  left_join(num_micro,by="host_genus_species") %>% 
+  left_join(num_api,by="host_genus_species") %>% 
+  left_join(num_bacteria_extra,by="host_genus_species") %>% 
+  left_join(num_cestode,by="host_genus_species") %>% 
+  left_join(num_fungi,by="host_genus_species") %>% 
+  left_join(num_nematode,by="host_genus_species") %>% 
+  left_join(num_trematode,by="host_genus_species")
 
 #calculate proportions of pathogen types
 decapod_hosts3 <- decapod_hosts2 %>% 
@@ -100,20 +137,8 @@ decapod_hosts3 <- decapod_hosts2 %>%
   mutate(prop_obligate=num_obligate/(num_obligate+num_opportunist)) %>% 
   mutate(prop_macro=num_macro/(num_macro+num_micro))
 
-#pathogen index
-decapod_hosts4 <- decapod_hosts3 %>% 
-  mutate(path_index=num_pathogens/num_results) %>% 
-  mutate(virus_index=num_viruses/num_results)
-
-#order by species name
-decapod_hosts5 <- decapod_hosts4 %>% 
-  arrange(host_genus_species)
-
-#limit to the 100 sp with pathogens
-decapod_hosts6 <- decapod_hosts5 %>% 
-  filter(!is.na(num_pathogens))
-
-#change sp with NA pathogens to 0 to include in 162 count
+###############
+#FUNCTION: if.na()
 if.na <- function(data){
   if(is.na(data)){
     output <- 0
@@ -122,16 +147,49 @@ if.na <- function(data){
   }
   return(output)
 }
-decapod_hosts7 <- decapod_hosts5 %>% 
+################
+
+#changes NAs to 0s for all EXCEPT num_pathogens
+decapod_hosts4 <- decapod_hosts3 %>%
+  rowwise() %>% 
+  mutate(num_viruses=if.na(num_viruses)) %>% 
+  mutate(num_isopods=if.na(num_isopods)) %>%
+  mutate(num_rhiz=if.na(num_rhiz)) %>% 
+  mutate(num_bacteria=if.na(num_bacteria)) %>% 
+  mutate(num_bacteria_intra=if.na(num_bacteria_intra)) %>% 
+  mutate(num_bacteria_extra=if.na(num_bacteria_extra)) %>% 
+  mutate(num_microsp=if.na(num_microsp)) %>% 
+  mutate(num_direct=if.na(num_direct)) %>% 
+  mutate(num_complex=if.na(num_complex)) %>% 
+  mutate(num_obligate=if.na(num_obligate)) %>% 
+  mutate(num_opportunist=if.na(num_opportunist)) %>% 
+  mutate(num_macro=if.na(num_macro)) %>% 
+  mutate(num_micro=if.na(num_micro)) %>% 
+  mutate(num_api=if.na(num_api)) %>%
+  mutate(num_cestode=if.na(num_cestode)) %>%
+  mutate(num_fungi=if.na(num_fungi)) %>%
+  mutate(num_nematode=if.na(num_nematode)) %>%
+  mutate(num_trematode=if.na(num_trematode)) %>%
+  mutate(prop_direct=if.na(prop_direct)) %>%
+  mutate(prop_obligate=if.na(prop_obligate)) %>%
+  mutate(prop_macro=if.na(prop_macro)) %>% 
+  arrange(host_genus_species)
+
+#limit to the 100 sp with pathogens
+decapod_hosts5 <- decapod_hosts4 %>% 
+  filter(!is.na(num_pathogens))
+
+#change sp with NA pathogens to 0 to include in 162 count
+decapod_hosts6 <- decapod_hosts4 %>% 
   rowwise() %>% 
   mutate(num_pathogens=if.na(num_pathogens))
 
 #save 100 sp to new file name
-write.csv(decapod_hosts6,"analyze_data/hosts_clean_pathcounts100.csv",
+write.csv(decapod_hosts5,"analyze_data/hosts_clean_pathcounts100.csv",
           row.names=FALSE)
 
 #save 162 sp to new file name
-write.csv(decapod_hosts7,"analyze_data/hosts_clean_pathcounts162.csv",
+write.csv(decapod_hosts6,"analyze_data/hosts_clean_pathcounts162.csv",
           row.names=FALSE)
 
 
